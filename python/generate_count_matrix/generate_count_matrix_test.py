294f92f0854c1c5361b0ca3037be34d009d330a5
Jessica Chung <jchung@unimelb.edu.au>
2017-06-01 22:13:13 +1000

    Add unit tests

--- python/generate_count_matrix/generate_count_matrix_test.py
+++ python/generate_count_matrix/generate_count_matrix_test.py
@@ -1,101 +1,57 @@
 '''
-Unit tests for generate-count-matrix.
+Unit tests for generate_count_matrix.
 
-Usage: python -m unittest -v generate-count-matrix_test
+Usage: python -m unittest -v generate_count_matrix_test
 '''
 
 import unittest
-from StringIO import StringIO
-from Bio import SeqIO
-from generate-count-matrix import FastaStats
+from generate_count_matrix import parse_counts
 
-class TestFastaStats(unittest.TestCase):
-    '''Unit tests for FastaStats'''
-    def do_test(self, input_str, minlen, expected):
-        "Wrapper function for testing FastaStats"
-        result = FastaStats().from_file(StringIO(input_str), minlen)
+class TestParseCounts(unittest.TestCase):
+    '''Unit tests for parse_counts'''
+    def do_test(self, data, expected, **kwargs):
+        "Wrapper function for testing parse_counts"
+        result = parse_counts(data, **kwargs)
         self.assertEqual(expected, result)
 
-    def test_zero_byte_input(self):
-        "Test input containing zero bytes"
-        expected = FastaStats(num_seqs=0,
-                              num_bases=0,
-                              min_len=None,
-                              max_len=None,
-                              average=None)
-        self.do_test('', 0, expected)
+    def test_basic_case(self):
+        "Test basic three line input"
+        expected = (("A","B","C"),[1,2,3])
+        self.do_test(data=["A\t1","B\t2","C\t3"], expected=expected)
 
-    def test_single_newline_input(self):
-        "Test input containing a newline (\n) character"
-        expected = FastaStats(num_seqs=0,
-                              num_bases=0,
-                              min_len=None,
-                              max_len=None,
-                              average=None)
-        self.do_test('\n', 0, expected)
+    def test_defined_columns(self):
+        "Test defined gene and count columns"
+        expected = (("A","B","C"),[1,2,3])
+        self.do_test(data=["1\tA\t0\t0\t1","2\tB\t0\t0\t2","3\tC\t0\t0\t3"],
+            expected=expected, gene_col=2, count_col=5)
 
-    def test_single_greater_than_input(self):
-        "Test input containing a single greater-than (>) character"
-        expected = FastaStats(num_seqs=1,
-                              num_bases=0,
-                              min_len=0,
-                              max_len=0,
-                              average=0)
-        self.do_test('>', 0, expected)
+    def test_delimiter(self):
+        "Test defined delimiter"
+        expected = (("A","B","C"),[1,2,3])
+        self.do_test(data=["1:A:0:0:1","2:B:0:0:2","3:C:0:0:3"],
+            expected=expected, gene_col=2, count_col=5, delimiter=":")
 
-    def test_one_sequence(self):
-        "Test input containing one sequence"
-        expected = FastaStats(num_seqs=1,
-                              num_bases=5,
-                              min_len=5,
-                              max_len=5,
-                              average=5)
-        self.do_test(">header\nATGC\nA", 0, expected)
+    def test_skip_lines(self):
+        "Test skipping header lines"
+        expected = (("A","B","C","D","E"),[1,2,3,4,5])
+        self.do_test(
+            data=["# header","# header","A\t1","B\t2","C\t3","D\t4","E\t5"],
+            expected=expected, skip_lines=2)
 
-    def test_two_sequences(self):
-        "Test input containing two sequences"
-        expected = FastaStats(num_seqs=2,
-                              num_bases=9,
-                              min_len=2,
-                              max_len=7,
-                              average=4)
-        self.do_test(">header1\nATGC\nAGG\n>header2\nTT\n", 0, expected)
+    def test_fail_nonidentical_fields(self):
+        "Files with different number of fields should exit on error"
+        with self.assertRaises(SystemExit):
+            parse_counts(data=["# header","A\t1","B\t2"])
 
-    def test_no_header(self):
-        "Test input containing sequence without preceding header"
-        expected = FastaStats(num_seqs=0,
-                              num_bases=0,
-                              min_len=None,
-                              max_len=None,
-                              average=None)
-        self.do_test("no header\n", 0, expected)
+    def test_fail_nonexistent_column(self):
+        "Using a non-existent column should exit on error"
+        with self.assertRaises(SystemExit):
+            parse_counts(data=["A\t1","B\t2","C\t3"], gene_col=3)
 
-    def test_minlen_less_than_all(self):
-        "Test input when --minlen is less than 2 out of 2 sequences"
-        expected = FastaStats(num_seqs=2,
-                              num_bases=9,
-                              min_len=2,
-                              max_len=7,
-                              average=4)
-        self.do_test(">header1\nATGC\nAGG\n>header2\nTT\n", 2, expected)
-
-    def test_minlen_greater_than_one(self):
-        "Test input when --minlen is less than 1 out of 2 sequences"
-        expected = FastaStats(num_seqs=1,
-                              num_bases=7,
-                              min_len=7,
-                              max_len=7,
-                              average=7)
-        self.do_test(">header1\nATGC\nAGG\n>header2\nTT\n", 3, expected)
-
-    def test_minlen_greater_than_all(self):
-        "Test input when --minlen is greater than 2 out of 2 sequences"
-        expected = FastaStats(num_seqs=0,
-                              num_bases=0,
-                              min_len=None,
-                              max_len=None,
-                              average=None)
-        self.do_test(">header1\nATGC\nAGG\n>header2\nTT\n", 8, expected)
+    def test_fail_nonnumerical_counts(self):
+        "Using a non-numerical count column should exit on error"
+        with self.assertRaises(SystemExit):
+            parse_counts(data=["A\tA","B\t2","C\t3"])
 
 
 if __name__ == '__main__':
